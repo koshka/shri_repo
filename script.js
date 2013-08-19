905db7217a6c226cb5f53e4869404039feb95451
koshka <koshka@users.noreply.github.com>
2013-08-19 21:35:04 +0400

    Hiding of some text added.

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('#popout').hide();
+  $('#trigger').click( function() {
+    $("#popout").slideToggle();
+     });
+});
+
+$(document).ready(function(){
+  $('#popout1').hide();
+  $('#trigger1').click( function() {
+    $("#popout1").slideToggle();
+     });
+});
